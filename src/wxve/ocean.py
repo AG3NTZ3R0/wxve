aa5242cbbf547ca278c5f5edb459d1dcf21a8d5c
blacy <brandon.lacy.50@gmail.com>
2022-03-03 16:18:05 -0500

    feat: added ocean.py

---
+++ src/wxve/ocean.py
@@ -0,0 +1,6 @@
+class Ocean:
+    """Save the Ocean!"""
+
+    def __init__(self):
+        """Initialize attributes."""
+        self.message = "10% of all proceeds are donated towards ocean conservation."
